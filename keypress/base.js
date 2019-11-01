e216999910315e92114807229c5cc269311fb5ce
Felipe G <pipe2705@gmail.com>
2019-10-31 22:14:55 -0700

    Finished the Space Bar Event Listener

--- keypress/base.js
+++ keypress/base.js
@@ -5,3 +5,31 @@ document.addEventListener("DOMContentLoaded", function(){
   // Handler when the DOM is fully loaded
 
 })
+
+let totalTime = document.querySelector("#total-time");
+let startTime = false; 
+let stopWatchStart = null; 
+let stopWatchStop = null; 
+let trackTime = function() {
+	document.body.onkeydown = function(bananas){
+		if(bananas.keyCode == 32){
+			if(startTime){
+				stopWatchStop = Date.now()
+					startTime = false
+					totalTime.textContent = (stopWatchStop - stopWatchStart) / 1000 + " secs"
+					console.log("Time's Up") 
+			}
+			else{
+				stopWatchStart = Date.now()
+				startTime = true;
+				console.log("Time Started!")
+			}
+		}
+	}
+}
+
+document.addEventListener("keypress", trackTime()) 
+
+
+
+
