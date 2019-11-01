cb85babefad1a913242653e40b3abbc2f765c1c4
Felipe G <pipe2705@gmail.com>
2019-10-31 17:05:42 -0700

    Finished click.js but did not finish base.js

--- click/base.js
+++ click/base.js
@@ -1,1 +1,22 @@
 console.log("Sanity Check: JS is working!");
+
+let ul = document.querySelector("ul")
+let li = document.createElement("li")
+let quotes = document.querySelectorAll("span")
+
+let imperative = document.getElementById('imperatives'); 
+
+
+for (quote of quotes){
+
+quote.addEventListener("click", (event) => {
+
+	let quote = event.target;
+
+	let newSpan = document.createElement("li"); 
+	newSpan.textContent = quote.textContent;
+
+	ul.append(newSpan)
+})
+
+}; 
