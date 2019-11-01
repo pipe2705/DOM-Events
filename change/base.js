1b2df8bcfa3247659aa7082f133d86e82d3a2004
Felipe G <pipe2705@gmail.com>
2019-10-31 17:31:45 -0700

    Finished change with help from jason :)

--- change/base.js
+++ change/base.js
@@ -1,4 +1,24 @@
 console.log("Sanity Check: JS is working!");
 
 let element = document.querySelector("#time")
-element.textContent = Date.now()
+element.textContent = Date.now();
+
+let x = document.querySelector("#left")
+
+let y = document.querySelector("#right")
+
+
+
+let total= document.querySelector("#total");
+let button = document.createElement("button")
+
+x.addEventListener('change', updateValue); 
+y.addEventListener('change', updateValue); 
+
+function updateValue(e) { 
+	total.value = Number(x.value) + Number(y.value)
+	}
+// get value is key.. 
+
+
+
